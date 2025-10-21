fee28d1d1fa23623636d02f2dd5dda2711581754
Liam Sullivan <lvsullivan07@gmail.com>
2025-10-21 10:58:06 -0500

    clean up oasis_loader, rename to load_oasis

--- dataloader.py
+++ dataloader.py
@@ -1,34 +1,30 @@
 import os
 import json
 import nibabel as nib
-import pandas as pd
 from tqdm import tqdm
-from scipy import sparse
 import sys
 
-def oasis_loader(filepath = None):
+def load_oasis(filename = "brain"):
 
-    images = []
+    images = {}
 
     with (open("localdata.json", 'r')) as json_file:
         oasis_path = json.load(json_file).get("oasis")
 
-    if filepath is None:
-        data_path = os.path.join(oasis_path, "data")
-        for folder in tqdm(os.listdir(data_path)):
-            dir = os.path.join(data_path, folder)
-            if os.path.isdir(dir):
-                img = nib.load(os.path.join(dir, "mri/aseg.mgz"))
-                if img.get_fdata(caching="unchanged").shape != (256, 256, 256):
-                    print("ERR")
-                # print(img.get_fdata())
-                # print(type(img.get_fdata()))
-                else:
-                    images.append(img)
+    data_path = os.path.join(oasis_path, "data")
 
-    print("OASIS images loaded (", sys.getsizeof(images), "bytes)", sep="")
+    for folder in tqdm(os.listdir(data_path), desc="OASIS"):
+        patient_dir = os.path.join(data_path, folder)
+        if os.path.isdir(patient_dir):
+            img = nib.load(os.path.join(patient_dir, f"mri/{filename}.mgz"))
+            if img.get_fdata(caching="unchanged").shape != (256, 256, 256):
+                raise Exception("Unexpected shape:", img.get_fdata(caching="unchanged").shape)
+            else:
+                images[folder] = img
+
+    print("OASIS images loaded (", sys.getsizeof(images), " bytes)", sep="")
 
     return images
 
 if __name__ == "__main__":
-    oasis_loader()
+    images = load_oasis()
