5b3a5fb8ad2d0514362e29830d1a51d49b7d0b5f
Liam Sullivan <lvsullivan07@gmail.com>
2025-11-19 09:38:57 -0600

    update correct_bias_fields and normalize

--- preprocessor.py
+++ preprocessor.py
@@ -1,7 +1,6 @@
 """
 10/4/25
 """
-from cProfile import label
 
 import pandas as pd
 import pickle
@@ -10,12 +9,35 @@ import time
 import sqlalchemy
 from tqdm import tqdm
 import numpy as np
-import matplotlib.pyplot as plt
 import SimpleITK as sitk
 import nibabel as nib
 from nilearn import datasets, image, plotting
 
-def normalize(sql_engine: sqlalchemy.engine.base.Engine, table: str = "raw") -> None:
+def correct_bias_fields(sql_engine: sqlalchemy.engine.base.Engine, table: str = "raw") -> None:
+    with sql_engine.connect() as conn:
+        data = pd.read_sql_query(f"SELECT * FROM {table}", conn)
+
+    corrector = sitk.N4BiasFieldCorrectionImageFilter()
+
+    for image_tuple in tqdm(data.itertuples(), total=len(data), desc="Correcting Bias Fields"):
+        orig_image = pickle.loads(image_tuple.image)
+        fdata = orig_image.get_fdata(caching="unchanged", dtype=np.float32)
+        image = sitk.GetImageFromArray(fdata)
+        mask_image = sitk.OtsuThreshold(image, 0, 1, 200)
+
+        corrected_image_itk = corrector.Execute(image, mask_image)
+        corrected_array = sitk.GetArrayFromImage(corrected_image_itk)
+
+        corrected_image = nib.freesurfer.mghformat.MGHImage(corrected_array, orig_image.affine, orig_image.header)
+
+        data.loc[image_tuple.Index, "image"] = pickle.dumps(corrected_image)
+
+    start_time = time.time()
+    print(f"Writing {len(data)} rows to file...", end="", flush=True)
+    data.to_sql(name="bias_corrected", con=sql_engine, if_exists="replace", index=False)
+    print(" DONE! (", np.round((time.time() - start_time) / 60, 2), " min)", sep="")
+
+def normalize(sql_engine: sqlalchemy.engine.base.Engine, table: str = "bias_corrected") -> None:
     """
     Normalizes all data from a SQL table using the selected engine
     :param sql_engine: The SQLAlchemy engine to use
@@ -24,119 +46,29 @@ def normalize(sql_engine: sqlalchemy.engine.base.Engine, table: str = "raw") -> 
     :type table: str
     :return: None
     """
-    with (sql_engine.connect() as conn):
-        data = pd.read_sql_query(f"SELECT * FROM {table} ORDER BY RANDOM() LIMIT 5", conn)
-
-        # def update_mean(mean, num_samples, new_num):
-        #     return ((mean * (num_samples - 1)) + new_num) / num_samples
-        #
-        # def update_stdev(sum_of_squares, square_of_sum, num_samples):
-        #     if num_samples == 1:
-        #         return 0
-        #     return np.sqrt((sum_of_squares / num_samples) - ((square_of_sum / num_samples) ** 2))
-        #
-        # num_batches = 16
-        # batch_size = np.ceil(len(data) / num_batches)
-        #
-        # means = []
-        # stdevs = []
-        # batch_sizes = []
-        #
-        # for batch_num in range(num_batches):
-        #     first = batch_num * batch_size
-        #     last = (batch_num + 1) * batch_size - 1 if batch_num < num_batches - 1 else len(data) - 1
-        #
-        #     mean = 0
-        #     stdev = 0
-        #     num_samples = 0
-        #     sum_of_squares = 0
-        #     sum_of_samples = 0
-        #
-        #     for image in tqdm(data.loc[first:last, "image"], desc=f"Batch {batch_num}"):
-        #         fdata = pickle.loads(image).get_fdata()
-        #         for row in fdata:
-        #             for col in row:
-        #                 for val in col:
-        #                     if val != 0:
-        #                         num_samples += 1
-        #                         sum_of_squares += val ** 2
-        #                         sum_of_samples += val
-        #                         mean = update_mean(mean, num_samples, val)
-        #                         stdev = update_stdev(sum_of_squares, sum_of_samples, num_samples)
-        #
-        #     means.append(mean)
-        #     stdevs.append(stdev)
-        #     batch_sizes.append(last - first)
-        #
-        # mean = sum([means[i] * batch_sizes[i] for i in range(len(means))]) / len(data)
-        # stdev =
-
-        fig, ax = plt.subplots(2, 1)
-        # ax[0].set_xlim(-0.5, 2)
-        ax[1].set_xlim(-0.5, 2)
-
-        for i, row in tqdm(data.iterrows(), total=len(data)):
-            # print(row["name"])
-            fdata = pickle.loads(row["image"]).get_fdata(caching="unchanged")
-            orig_shape = fdata.shape
-            masked_data = np.ma.masked_equal(fdata.flatten(), 0)
-            # print(masked_data.min(), masked_data.max())
-            counts, bins = np.histogram(masked_data.compressed(), bins=np.linspace(0, 125, 400))
-            ax[0].stairs(counts, bins)
-            mean = np.mean(masked_data)
-            stdev = np.std(masked_data)
-            # print(mean, stdev)
-            # print(data)
-            masked_data -= mean
-            masked_data /= stdev
-            # print(data)
-            # print(data.shape)
-            counts, bins = np.histogram(masked_data.compressed(), bins=np.linspace(-0.2, 12, 400))
-            ax[1].stairs(counts, bins)
-            # print(min(data), max(data))
-            # print(counts, bins)
-
-        plt.show()
-
-        # norm_data = pd.DataFrame(columns=["name", "image"])
-        #
-        # for image_tuple in tqdm(data.itertuples(), desc="Normalizing images"):
-        #     fdata = pickle.loads(image_tuple.image).get_fdata()
-        #     for x in tqdm(range(len(fdata)), leave=False):
-        #         for y in range(len(fdata[x])):
-        #             for z in range(len(fdata[x][y])):
-        #                 value = fdata[x][y][z]
-        #                 if value != 0:
-        #                     value -= mean
-        #                     value /= stdev
-        #                     fdata[x][y][z] = value
-        #
-        #     norm_data[len(norm_data)] = [image_tuple.name, fdata]
-        #
-        # start_time = time.time()
-        # print("Writing to file...", end="", flush=True)
-        # # norm_data.to_sql(name="preprocessed", con=sql_engine, if_exists="replace", index=False)
-        # print(" DONE! (", np.round((time.time() - start_time) / 60, 2), " min)", sep="")
-
-def correct_bias_fields(sql_engine: sqlalchemy.engine.base.Engine, table: str = "raw") -> None:
     with sql_engine.connect() as conn:
         data = pd.read_sql_query(f"SELECT * FROM {table}", conn)
+    print(len(data))
 
-    corrector = sitk.N4BiasFieldCorrectionImageFilter()
+    for image_tuple in tqdm(data.itertuples(), total=len(data), desc="Normalizing Images"):
+        fdata = pickle.loads(image_tuple.image).get_fdata(caching="unchanged")
+        orig_shape = fdata.shape
+        masked_data = np.ma.masked_equal(fdata.flatten(), 0)
+        mean = np.mean(masked_data)
+        stdev = np.std(masked_data)
+        masked_data -= mean
+        masked_data /= stdev
 
-    for image_tuple in tqdm(data.itertuples(), total=len(data)):
-        fdata = pickle.loads(image_tuple.image).get_fdata()
-        image = sitk.GetImageFromArray(fdata)
-        mask_image = sitk.OtsuThreshold(image, 0, 1, 200)
+        norm_image = masked_data.reshape(orig_shape).filled(fill_value=0)
+        data.loc[image_tuple.Index, "image"] = norm_image
 
-        corrected_image = corrector.Execute(image, mask_image)
-        corrected_array = sitk.GetArrayFromImage(corrected_image)
+    start_time = time.time()
+    print(f"Writing {len(data)} rows to file...", end="", flush=True)
+    data.to_sql(name="normalized", con=sql_engine, if_exists="replace", index=False)
+    print(" DONE! (", np.round((time.time() - start_time) / 60, 2), " min)", sep="")
 
-        data.loc[image_tuple.Index, "image"] = corrected_array
-
-    data.to_sql(name="preprocessed", con=sql_engine, if_exists="replace", index=False)
-
-def map_to_mni(sql_engine: sqlalchemy.engine.base.Engine, table: str = "raw", dimensions: tuple = (256, 256, 256)) -> None:
+def map_to_mni(sql_engine: sqlalchemy.engine.base.Engine, table: str = "normalized",
+               dimensions: tuple = (256, 256, 256)) -> None:
     with sql_engine.connect() as conn:
         data = pd.read_sql_query(f"SELECT * FROM {table} ORDER BY RANDOM() LIMIT 3", conn)
 
