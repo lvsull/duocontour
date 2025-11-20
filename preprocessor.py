b8d412ee0ccb79feff03678e6976b0b156a49a1c
Liam Sullivan <lvsullivan07@gmail.com>
2025-11-20 09:48:56 -0600

    update docstrings

--- preprocessor.py
+++ preprocessor.py
@@ -3,12 +3,11 @@
 """
 
 import pandas as pd
-import pickle
-import time
-import os
-import shutil
-import json
-
+from pickle import dumps, loads
+from time import time
+from os import path, mkdir
+from shutil import rmtree
+from json import load
 import sqlalchemy
 from tqdm import tqdm
 import numpy as np
@@ -17,21 +16,30 @@ import nibabel as nib
 from nilearn import datasets, image, plotting
 
 def correct_bias_fields(sql_engine: sqlalchemy.engine.base.Engine, table: str = "raw") -> None:
+    """
+    Corrects bias fields of images stored on a SQL table
+    :param sql_engine: The SLQLAlchemy engine to use
+    :type sql_engine: sqlalchemy.engine.base.Engine
+    :param table: *optional, default "raw"* The name of the table to use
+    :type table: str
+    :return: None
+    :rtype: NoneType
+    """
     with sql_engine.connect() as conn:
         data = pd.read_sql_query(f"SELECT * FROM {table}", conn)
 
     corrector = sitk.N4BiasFieldCorrectionImageFilter()
 
     with open("localdata.json") as json_file:
-        save_path = json.load(json_file).get("bias_corrected")
+        save_path = load(json_file).get("bias_corrected")
     try:
-        shutil.rmtree(save_path)
+        rmtree(save_path)
     except FileNotFoundError:
         pass
-    os.mkdir(save_path)
+    mkdir(save_path)
 
     for image_tuple in tqdm(data.itertuples(), total=len(data), desc="Correcting Bias Fields"):
-        orig_image = pickle.loads(image_tuple.image)
+        orig_image = loads(image_tuple.image)
         fdata = orig_image.get_fdata(caching="unchanged")
         image = sitk.GetImageFromArray(fdata)
         mask_image = sitk.OtsuThreshold(image, 0, 1, 200)
@@ -41,33 +49,34 @@ def correct_bias_fields(sql_engine: sqlalchemy.engine.base.Engine, table: str = 
 
         corrected_image = nib.Nifti1Image(corrected_array, orig_image.affine, orig_image.header)
 
-        filepath = os.path.join(save_path, f"{image_tuple.name}.nii.gz")
+        filepath = path.join(save_path, f"{image_tuple.name}.nii.gz")
         nib.save(corrected_image, filepath)
 
         corrected_image_view = nib.load(filepath)
 
-        data.loc[image_tuple.Index, "image"] = pickle.dumps(corrected_image_view)
+        data.loc[image_tuple.Index, "image"] = dumps(corrected_image_view)
 
-    start_time = time.time()
+    start_time = time()
     print(f"Writing {len(data)} rows to file...", end="", flush=True)
     data.to_sql(name="bias_corrected", con=sql_engine, if_exists="replace", index=False)
-    print(" DONE! (", np.round((time.time() - start_time) / 60, 2), " min)", sep="")
+    print(" DONE! (", np.round((time() - start_time) / 60, 2), " min)", sep="")
 
 def normalize(sql_engine: sqlalchemy.engine.base.Engine, table: str = "bias_corrected") -> None:
     """
     Normalizes all data from a SQL table using the selected engine
     :param sql_engine: The SQLAlchemy engine to use
     :type sql_engine: sqlalchemy.engine.base.Engine
-    :param table: *optional, default "raw"* The name of the table to normalize
+    :param table: *optional, default "bias_corrected"* The name of the table to normalize
     :type table: str
     :return: None
+    :rtype: NoneType
     """
     with sql_engine.connect() as conn:
         data = pd.read_sql_query(f"SELECT * FROM {table}", conn)
     print(len(data))
 
     for image_tuple in tqdm(data.itertuples(), total=len(data), desc="Normalizing Images"):
-        fdata = pickle.loads(image_tuple.image).get_fdata(caching="unchanged")
+        fdata = loads(image_tuple.image).get_fdata(caching="unchanged")
         orig_shape = fdata.shape
         masked_data = np.ma.masked_equal(fdata.flatten(), 0)
         mean = np.mean(masked_data)
@@ -78,10 +87,10 @@ def normalize(sql_engine: sqlalchemy.engine.base.Engine, table: str = "bias_corr
         norm_image = masked_data.reshape(orig_shape).filled(fill_value=0)
         data.loc[image_tuple.Index, "image"] = norm_image
 
-    start_time = time.time()
+    start_time = time()
     print(f"Writing {len(data)} rows to file...", end="", flush=True)
     data.to_sql(name="normalized", con=sql_engine, if_exists="replace", index=False)
-    print(" DONE! (", np.round((time.time() - start_time) / 60, 2), " min)", sep="")
+    print(" DONE! (", np.round((time() - start_time) / 60, 2), " min)", sep="")
 
 def map_to_mni(sql_engine: sqlalchemy.engine.base.Engine, table: str = "normalized",
                dimensions: tuple = (256, 256, 256)) -> None:
@@ -105,7 +114,7 @@ def map_to_mni(sql_engine: sqlalchemy.engine.base.Engine, table: str = "normaliz
     image_df = pd.DataFrame(columns=["name", "image"])
 
     for image_tuple in data.itertuples():
-        subject_image = pickle.loads(image_tuple.image)
+        subject_image = loads(image_tuple.image)
         mapped_image = image.resample_to_img(subject_image, mni_template, force_resample=True, copy_header=True)
         image_df.loc[len(image_df)] = [image_tuple.name, mapped_image.get_fdata()]
         plotting.plot_img(subject_image, cut_coords=(0, 0, 0))
