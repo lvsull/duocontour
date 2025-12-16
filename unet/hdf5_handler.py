f6b15316aa457d8f771c04e53cdbb017986d79ee
Liam Sullivan <lvsullivan07@gmail.com>
2025-12-15 18:50:54 -0600

    add labels to save_images

--- unet/hdf5_handler.py
+++ unet/hdf5_handler.py
@@ -4,9 +4,21 @@ import h5py
 from tqdm import tqdm
 from shutil import rmtree
 from os import mkdir
+import sqlalchemy
 
-def save_images(engine, save_path):
-    data = pd.read_sql_query("SELECT * FROM preprocessed", engine)
+
+def save_images(sql_engine: sqlalchemy.engine.base.Engine, save_path: str) -> None:
+    """
+    Saves images and label data to HDF5 files as required by pytorch3dunet
+    :param sql_engine: The SQLAlchemy engine to use
+    :type sql_engine: sqlalchemy.engine.base.Engine
+    :param save_path: The directory to save HDF5 tables to
+    :type save_path: str
+    :return: None
+    :rtype: NoneType
+    """
+    raw_data = pd.read_sql_query("SELECT * FROM preprocessed", sql_engine)
+    label_data = pd.read_sql_query("SELECT * FROM label", sql_engine)
 
     try:
         rmtree(save_path)
@@ -14,7 +26,13 @@ def save_images(engine, save_path):
         pass
     mkdir(save_path)
 
-    for row in tqdm(data.itertuples(), total=len(data), desc="Saving images to HDF5"):
-        image = pickle.loads(row.image).get_fdata()
-        with h5py.File(f"{save_path}/{row.name}.hdf5", 'w') as f:
-            f.create_dataset('raw', data=image)
+    for row in tqdm(range(len(raw_data)), desc="Saving images to HDF5"):
+        raw = pickle.loads(raw_data.loc[row, "image"]).get_fdata()
+        label = pickle.loads(label_data.loc[row, "image"]).get_fdata()
+
+        if not raw.shape == label.shape:
+            raise Exception("Some images do not have the same shape")
+
+        with h5py.File(f"{save_path}/{raw_data.loc[row, "name"]}.hdf5", 'w') as f:
+            f.create_dataset("raw", data=raw)
+            f.create_dataset("label", data=label)
