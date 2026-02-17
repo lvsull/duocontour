c03a291b262b7f024c8ff7f4606065a9e9e488f8
lvsull <lvsullivan07@gmail.com>
2026-02-17 16:54:42 -0600

    add config setup

--- setup.py
+++ setup.py
@@ -3,38 +3,53 @@
 Setup functions
 """
 
-def create_config() -> bool:
-    """
-    Create an empty config file with default labels
-    :return: ``True`` if file is written, ``False`` otherwise (e.g. if config file already exists)
-    :rtype: bool
-    """
-    config = ("log_file:\n"
-              "database:\n"
-              "mni_template:\n"
-              "raw:\n\n"
-              "padded_image:\n"
-              "mni_registered_image:\n"
-              "bias_corrected_image:\n"
-              "preprocessed_image:\n\n"
-              "padded_label:\n"
-              "mni_registered_label:\n"
-              "imputed_label:\n"
-              "preprocessed_label:\n\n"
-              "unet:\n"
-              "  train:\n"
-              "  validation:\n"
-              "  train_config:\n"
-              "  test_config:\n")
+import yaml
 
-    try:
-        with open("config.yaml", "x") as f:
-            f.write(config)
-        return True
-    except FileExistsError:
-        pass
+def add_to_line(initial: str, new: str) -> str:
+    return initial.strip("\n") + " " + new + "\n"
 
-    return False
+def fill_config():
+    with open("config.yaml", "r") as f:
+        lines = f.readlines()
+
+    print("INPUT EACH DIRECTORY OR FILE PATH")
+
+    for i in range(len(lines)):
+        if lines[i] == "\n":
+            print()
+            continue
+        try:
+            if lines[i+1].startswith("  ") and not lines[i].startswith("  "):
+                continue
+        except IndexError:
+            pass
+
+        value = input(lines[i].strip() + " ")
+        lines[i] = add_to_line(lines[i], value)
+
+    with open("config.yaml", "w") as f:
+        f.writelines(lines)
+
+
+def fill_train_config():
+    with open("config.yaml", "r") as f:
+        unet_conf = yaml.safe_load(f)["unet"]
+        config_path = unet_conf["train_config"]
+        checkpoint_path = unet_conf["checkpoint"]
+        train_path = unet_conf["train"]
+        val_path = unet_conf["validation"]
+
+    with open(config_path, "r") as f:
+        lines = f.readlines()
+
+    lines[23] = add_to_line(lines[23], checkpoint_path)
+    lines[86] = add_to_line(lines[86], train_path)
+    lines[136] = add_to_line(lines[136], val_path)
+
+    with open(config_path, "w") as f:
+        f.writelines(lines)
+
 
 if __name__ == "__main__":
-    create_config()
+    fill_config()
+    fill_train_config()
