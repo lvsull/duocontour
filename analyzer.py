67da75829bc1a93e05d2e6fca19a2f5f5a4d36f9
lvsull <lvsullivan07@gmail.com>
2026-03-02 09:04:38 -0600

    graph loss vs eval score

--- analyzer.py
+++ analyzer.py
@@ -1,5 +1,7 @@
 import re
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def log_to_csv(log_file: str, train_output_path: str, val_output_path: str):
     with open(log_file, 'r') as f:
@@ -12,7 +14,7 @@ def log_to_csv(log_file: str, train_output_path: str, val_output_path: str):
         filtered_lines = [line for line in lines if re.search(keyword, line)]
         iter = 0
         for line in filtered_lines:
-            iter += 5000 if keyword == "Training" else 10000
+            iter += 5000 if keyword == "Training" else 25000
             search = re.search(r".*Loss: (\d*.\d*).*Evaluation score: (\d*.\d*)", line)
             if search:
                 df.loc[len(df)] = [iter, float(search.group(1)), float(search.group(2))]
@@ -23,3 +25,32 @@ def log_to_csv(log_file: str, train_output_path: str, val_output_path: str):
 
     train_lines.to_csv(train_output_path, index=False)
     val_lines.to_csv(val_output_path, index=False)
+
+
+def graph_loss_score(csv_file: str, ax1, ax2, title: str = "", y_label: bool = False):
+    data = pd.read_csv(csv_file)
+    # data = data.iloc[:, ]
+    sns.lineplot(data, x="Iterations", y="Loss", color="b", ax=ax1)
+    sns.lineplot(data, x="Iterations", y="Score", color="r", ax=ax2)
+    ax1.set_yscale("log")
+    ax1.set_title(title)
+    if not y_label:
+        ax1.set_ylabel(None)
+        ax2.set_yticklabels([])
+        ax2.set_ylabel(None)
+    ax2.set_ylim([0, 1])
+
+
+if __name__ == "__main__":
+    log_to_csv("info_2.log", "output/train.csv", "output/val.csv")
+    sns.set_theme(style="whitegrid")
+    sns.set_color_codes("bright")
+    f, axs = plt.subplots(2, 2, sharex=True)
+    f.set_dpi(600)
+    f.set_size_inches(8, 8)
+    graph_loss_score("output/train.csv", axs[0, 0], axs[1, 0], "Training", y_label=True)
+    graph_loss_score("output/val.csv", axs[0, 1], axs[1, 1], "Validation")
+    plt.suptitle("Loss and Evaluation Score vs Iteration for Training and Validation", size=16)
+    f.tight_layout()
+    plt.savefig("output/loss_score.png", dpi=600)
+    plt.show()
